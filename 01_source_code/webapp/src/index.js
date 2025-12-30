b93bc9436520f8d454cd812291e068639507e88c
admin-cbo <lam01218956936@gmail.com>
2025-12-30 04:36:15 +0000

    Day58:fix username Build & push Doker image to DockerHubs,add CD,fix Deployss

--- 01_source_code/webapp/src/index.js
+++ 01_source_code/webapp/src/index.js
@@ -1,2 +1,16 @@
+// index.js
 console.log("Hello CI/CD Project - Day 55");
+
+// Thêm server đơn giản để giữ container chạy
+const express = require('express');
+const app = express();
+const PORT = 3003;
+
+app.get('/', (req, res) => {
+  res.send('Hello CI/CD Project - Day 55');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
